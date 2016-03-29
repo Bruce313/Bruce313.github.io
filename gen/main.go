6afa87ca6f60d90182bb47697711655f8da78e3c
LiuBing <liubing@xiaohongchun.com>
2016-03-29 17:09:36 +0800

    save

--- gen/main.go
+++ gen/main.go
@@ -1,19 +1,97 @@
 package main
 
 import (
-	"fmt"
+    "github.com/satori/go.uuid"
 	"io/ioutil"
 	"log"
+    "path"
+    "os"
 )
 
 const md_DIR = "./md"
 
 func main() {
-	files, err := ioutil.ReadDir(md_DIR)
+    //(md files in md dir) -> md structs
+    mds, err := ParseMd(md_DIR)
+    if err != nil {
+        log.Fatal(`parse all md struct:`, err)
+    }
+    //generate html files of `content html`
+    for _, m := range mds {
+        html := m.Html()
+        htmlName := uuid.V4()
+    }
+}
+
+func ParseMd(mdDir string) (mds []Md, err error) {
+	files, err := ioutil.ReadDir(mdDir)
 	if err != nil {
-		log.Fatal("read dir:", err)
+        return
 	}
+    var mds []Md
 	for _, f := range files {
-		fmt.Printf("read file %s\n", f.Name())
+        var cate string //dir > @(cate)
+		if f.IsDir() {
+            mds = append(mds, ParseDirInMdDir(path.Join(mdDir, f.Name()))...)
+            continue
+        }
+        regularFile, err := os.Open(f)
+        if err != nil {
+            return
+        }
+        content, err := ioutil.ReadAll(regularFile)
+        cate = findCate(content)
+        tags := findTags(content)
+        mds = append(mds, Md {
+            Cate: cate,
+            Tags: tags,
+            Content: content,
+        })
+        regularFile.Close()
 	}
+}
+
+func ParseDirInMdDir(p string) (mds []Md, err error) {
+    mdFiles, err := getFileInDir(p)
+    if err != nil {
+        return
+    }
+    for _, mf := range mdFiles {
+        f, err := os.Open(mf)
+        if err != nil {
+            return
+        }
+        content, err := ioutil.ReadAll(f)
+        if err != nil {
+            return
+        }
+        f.Close()
+        tags := findTags(content)
+        mds = append(mds, Md {
+            Cate: path.Base(p),
+            Tags: tags,
+            Content: content,
+        })
+    }
+    return
+}
+
+func getFileInDir(dirName string) (files []string, err error) {
+    fs, err := ioutil.ReadDir(dirName)
+    if err != nil {
+        return
+    }
+    for _, f := range fs {
+        pathNow := path.Join(dirName, f.Name())
+        if f.IsDir() {
+            fsTmp, err := getFileInDir(pathNow)
+            if err != nil {
+                return
+            }
+            files = append(files, fsTmp...)
+            continue
+        }
+        files = append(files, pathNow)
+    }
+    return
 }
--- gen/md.go
+++ gen/md.go
@@ -1,18 +1,38 @@
 package main
 
+import (
+	"regexp"
+)
+
+
+
 type Md struct {
 	Tags    []string
 	Cate    string
-	content []byte
+	Content []byte
 }
+
+var (
+	reCate = regexp.MustCompile("@\\((\\w+)\\)")
+	reTags = regexp.MustCompile("@\\(\\w+\\)\\s*(\\[\\w+\\])+")
+)
 
 func (self *Md) Html() []byte {
+	return nil
 }
 
-func NewMd(fileName string) (m *Md, err error) {
-	content, err := ioutil.ReadFile(fileName)
-	if err != nil {
+func findCate(content string) (cate string) {
+	f := reCate.FindStringSubmatch(content)
+	if len(f) < 2 {
 		return
 	}
-	cates := re
+	return f[1]
+}
+
+func findTags(content string) (tags []string) {
+	out := reTags.FindStringSubmatch(content)[1:]
+	for _, v := range out {
+		tags = append(tags, v[1:len(v)-1])
+	}
+	return
 }
